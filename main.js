a5616f53060a117b7f7a4bc000481251ff070521
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-10 14:50:50 -0500

    finish

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const para = document.querySelector('p')
 para.style.color = 'lightBlue';
-const head = document.querySelector('head')
+const head = document.querySelector('h1')
 head.style.fontSize = '5em'
 para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
 
